2f3b0e1295f039be5f919ddbf5371dd793fa9382
Preethi Srivarnas <srinivp@miamioh.edu>
2008-12-09 19:45:36 +0000

    This class represents the rationale element.

---
+++ src/edu/wpi/cs/jburge/SEURAT/views/rationaleElement.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs.jburge.SEURAT.views;
+
+
+public class rationaleElement {
+
+	public String elementType;
+	public String elementContents;
+	
+	public rationaleElement(String t)
+	{
+		elementType=t;
+		elementContents="";
+	}
+}
