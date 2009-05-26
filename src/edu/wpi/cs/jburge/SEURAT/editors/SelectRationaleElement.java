ef30d692b1b8add5d099a88e8e0344f3cab71618
Janet Burge <burgeje@miamioh.edu>
2009-05-26 19:29:40 +0000

    Added ability to choose new parents for elements

---
+++ src/edu/wpi/cs/jburge/SEURAT/editors/SelectRationaleElement.java
@@ -0,0 +1,223 @@
+package edu.wpi.cs.jburge.SEURAT.editors;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleDB;
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleElement;
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleElementType;
+
+/**
+ * Presents the user with a list of rationale elements of a specific type to
+ * select from. The user also has the option of searching for an element in the list
+ * and can add new items.
+ */
+public class SelectRationaleElement {
+
+	/**
+	 * Points back to the display
+	 */
+	private Display ourDisplay;
+	/**
+	 * Points to the shell
+	 */
+	private Shell shell;
+	/**
+	 * The type of rationale element that is being selected
+	 */
+	private RationaleElementType type;
+	/**
+	 * The text box where the user can enter a search string
+	 */
+	private Text searchText;
+	/**
+	 * The list of entities that the user can select from
+	 */
+	private List entityList;
+	
+	/**
+	 * The string typed into the text box (searchText)
+	 */
+	String selectionString;
+	/**
+	 * The index to the selected item
+	 */
+	int selectionIndex;
+	
+	/**
+	 * The selected item
+	 */
+	private RationaleElement ourItem;
+	
+	
+	/**
+	 * Displays a list of rationale elements that the user can select from
+	 * @param display - points to the display
+	 * @param eType - the **child** rationale element type
+	 */
+	public SelectRationaleElement(Display display, RationaleElementType eType) {
+		
+		//set up our display
+		ourDisplay = display;
+		type = eType;
+		
+		
+		
+		ourItem = null;
+		selectionIndex = 0;
+		
+		shell = new Shell(display, SWT.DIALOG_TRIM | SWT.PRIMARY_MODAL);
+		shell.setText("Select New Parent Item");
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 5;
+		gridLayout.makeColumnsEqualWidth = true;
+		shell.setLayout(gridLayout);
+		
+//		new Label(shell, SWT.NONE).setText("Entity Type:");
+		entityList = new List(shell, SWT.SINGLE | SWT.V_SCROLL | SWT.H_SCROLL);
+		GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalSpan = 5;
+		
+		RationaleDB db = RationaleDB.getHandle();
+		Vector listV = db.getRationaleElementList(type);
+		Enumeration listE = listV.elements();
+		while (listE.hasMoreElements())
+		{
+			entityList.add( (String) listE.nextElement());
+		}
+		
+//		int listHeight = entityList.getItemHeight() * 12;
+//		Rectangle trim = entityList.computeTrim(0, 0, 0, listHeight);
+//		gridData.heightHint = trim.height;
+		DisplayUtilities.setListDimensions(entityList, gridData, 12, 80);
+		entityList.setLayoutData(gridData);
+		
+		//now, our search controls
+		new Label(shell, SWT.NONE).setText("Search:");	
+		
+		//and our text to enter
+		searchText = new Text(shell, SWT.SINGLE | SWT.BORDER);
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalSpan = 4;
+		searchText.setLayoutData(gridData);	
+		
+		new Label(shell, SWT.NONE).setText(" ");	
+		new Label(shell, SWT.NONE).setText(" ");	
+		
+		Button searchB = new Button(shell, SWT.PUSH);
+		searchB.setText("Search");
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		Point buttonSize = searchB.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+//		gridData.horizontalSpan = 3;
+		searchB.setLayoutData(gridData);
+		searchB.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event)
+			{
+				int limit = entityList.getItemCount();
+//				System.out.println("number items = " + Integer.toString(limit));
+				boolean found = false;
+				int numSearched = 0;
+				
+				selectionString = searchText.getText();
+				while ((!found) && (numSearched < limit))
+				{
+					String name = entityList.getItem(selectionIndex);
+					if (name.lastIndexOf(selectionString) >= 0)
+					{
+//						System.out.println("name = " + name);
+//						System.out.println("selection = " + selectionString);
+//						System.out.println("comparison = " + name.lastIndexOf(selectionString));
+						found = true;
+						entityList.select(selectionIndex);
+						entityList.setTopIndex(selectionIndex);
+						selectionIndex++;
+					}
+					else
+					{
+						selectionIndex++;
+					}
+					if (selectionIndex >= limit)
+					{
+						selectionIndex = 0;
+					}
+					numSearched++;
+				}
+			}
+		});
+		
+		new Label(shell, SWT.NONE).setText(" ");	
+		new Label(shell, SWT.NONE).setText(" ");
+		new Label(shell, SWT.NONE).setText(" ");	
+		new Label(shell, SWT.NONE).setText(" ");
+		
+
+			new Label(shell, SWT.NONE).setText(" ");
+		Button editB = new Button(shell, SWT.PUSH);
+		editB.setText("Select");
+//		editB.setSize(buttonSize);
+		gridData = new GridData(GridData.VERTICAL_ALIGN_BEGINNING| GridData.HORIZONTAL_ALIGN_FILL);
+//		gridData.horizontalIndent = 5;
+		gridData.widthHint = buttonSize.y;
+		editB.setLayoutData(gridData);
+		if (eType==RationaleElementType.ALTERNATIVE)
+			type=RationaleElementType.DECISION;
+		if (eType==RationaleElementType.DECISION)
+			type=RationaleElementType.ALTERNATIVE;
+		editB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				
+				String name = entityList.getItem(entityList.getSelectionIndex());
+				ourItem  = RationaleDB.getRationaleElement(name, type);				
+				shell.close();
+				shell.dispose();
+			}
+		});
+		
+		
+		Button cancelB = new Button(shell, SWT.PUSH);
+		cancelB.setText("Cancel");
+		GridData gridData2 = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		//	cancelB.setSize(searchB.getSize());
+//		gridData2.horizontalIndent = 5;
+		cancelB.setLayoutData(gridData2);
+		cancelB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				shell.close();
+				shell.dispose();
+				
+			}
+		});
+		
+		
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		
+	}
+	
+	/**
+	 * Get the item that has been selected.
+	 * @return the selected rationale element
+	 */
+	public RationaleElement getNewItem()
+	{
+		return ourItem;
+	}
+}
---
+++ src/edu/wpi/cs/jburge/SEURAT/editors/SelectRationaleElement_Treeview.java
@@ -0,0 +1,269 @@
+package edu.wpi.cs.jburge.SEURAT.editors;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import edu.wpi.cs.jburge.SEURAT.rationaleData.OntEntry;
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleDB;
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleElement;
+import edu.wpi.cs.jburge.SEURAT.rationaleData.RationaleElementType;
+
+public class SelectRationaleElement_Treeview {
+	
+	/**
+	 * Points back to the display
+	 */
+	private Display ourDisplay;
+	/**
+	 * Points to the shell
+	 */
+	private Shell shell;
+	
+	/**
+	 * The tree of ontology entries that form the argument ontology
+	 */
+	private Tree ontologyTree;
+	/**
+	 * The root element of the tree
+	 */
+	private TreeItem root;
+	
+	/**
+	 * The selected item from the tree
+	 */
+	private RationaleElement selElement;
+	
+	/**
+	 * The type of selected item
+	 */
+	private RationaleElementType eleType;
+	
+	
+	/**
+	 * Used to display the argument ontology tree so an element can be selected
+	 * @param display - points back to the display
+	 * @param type - the type of the element being selected to move around the tree
+	 */
+	public SelectRationaleElement_Treeview(Display display,RationaleElementType type) {
+		
+		this.eleType=type;
+		ourDisplay = display;
+		shell = new Shell(display, SWT.DIALOG_TRIM | SWT.PRIMARY_MODAL);
+		shell.setText("Select Rationale Element");
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		gridLayout.makeColumnsEqualWidth = true;
+		shell.setLayout(gridLayout);
+	
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+		ontologyTree = new Tree(shell, SWT.SINGLE | SWT.V_SCROLL| SWT.H_SCROLL);
+		
+		root = new TreeItem(ontologyTree, SWT.NONE);
+		
+		if (type==RationaleElementType.ALTERNATIVE)
+		{
+			root.setText("Decision");
+			populateTree(root, "NotNull", RationaleElementType.DECISION,true);
+		}
+		if (type==RationaleElementType.REQUIREMENT)
+		{
+			root.setText(type.toString());
+			populateTree(root, "NotNull", type,true);
+		}
+		
+		
+		GridData gridData = new GridData( GridData.VERTICAL_ALIGN_BEGINNING | GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalSpan = 3;
+		gridData.heightHint = ontologyTree.getItemHeight() * 15;
+		ontologyTree.setLayoutData(gridData);
+		
+		new Label(shell, SWT.NONE).setText("");		
+		
+		Button findB = new Button(shell, SWT.PUSH);
+		findB.setText("Select");
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_BEGINNING);
+		gridData.horizontalIndent = 5;
+		findB.setLayoutData(gridData);
+		if (eleType==RationaleElementType.ALTERNATIVE)
+			eleType=RationaleElementType.DECISION;
+		findB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				
+				TreeItem[] selected = ontologyTree.getSelection();
+				selElement = new RationaleElement();
+				selElement  = RationaleDB.getRationaleElement(selected[0].getText(), eleType);	
+				
+				shell.close();
+				shell.dispose();
+				
+			}
+		});
+		
+		Button cancelB = new Button(shell, SWT.PUSH);
+		cancelB.setText("Cancel");
+		GridData gridData2 = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_BEGINNING);
+		gridData2.horizontalIndent = 5;
+		cancelB.setLayoutData(gridData2);
+		cancelB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				selElement = null;
+				shell.close();
+				shell.dispose();
+				
+			}
+		});
+		
+		//We want the buttons to be of equal size...
+		findB.setSize(cancelB.getSize());
+		
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		
+	}
+	
+	/**
+	 * This method is for moving decision and argument to different type of element on the tree
+	 * @param display - points back to the display
+	 * @param type - the type of the element being selected to move around the tree
+	 * @param ptype - the type parent element
+	 */
+	public SelectRationaleElement_Treeview(Display display,RationaleElementType ele, RationaleElementType ptype) {
+		
+		this.eleType=ptype;
+		ourDisplay = display;
+		shell = new Shell(display, SWT.DIALOG_TRIM | SWT.PRIMARY_MODAL);
+		shell.setText("Select Rationale Element");
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		gridLayout.makeColumnsEqualWidth = true;
+		shell.setLayout(gridLayout);
+	
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+		ontologyTree = new Tree(shell, SWT.SINGLE | SWT.V_SCROLL| SWT.H_SCROLL);
+		
+		root = new TreeItem(ontologyTree, SWT.NONE);
+		
+		if (ptype==RationaleElementType.ALTERNATIVE)
+		{
+			root.setText("Alternative");
+			populateTree(root, "NotNull", ptype,true);
+		}
+		if (ptype==RationaleElementType.DECISION)
+		{
+			root.setText("Decision");
+			populateTree(root, "NotNull", ptype,true);
+		}
+		if (ptype==RationaleElementType.REQUIREMENT)
+		{
+			root.setText("Requirement");
+			populateTree(root, "NotNull", ptype,true);
+		}
+		
+		
+		GridData gridData = new GridData( GridData.VERTICAL_ALIGN_BEGINNING | GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalSpan = 3;
+		gridData.heightHint = ontologyTree.getItemHeight() * 15;
+		ontologyTree.setLayoutData(gridData);
+		
+		new Label(shell, SWT.NONE).setText("");		
+		
+		Button findB = new Button(shell, SWT.PUSH);
+		findB.setText("Select");
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_BEGINNING);
+		gridData.horizontalIndent = 5;
+		findB.setLayoutData(gridData);
+		findB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				
+				TreeItem[] selected = ontologyTree.getSelection();
+				selElement = new RationaleElement();
+				selElement  = RationaleDB.getRationaleElement(selected[0].getText(), eleType);	
+				shell.close();
+				shell.dispose();
+				
+			}
+		});
+		
+		Button cancelB = new Button(shell, SWT.PUSH);
+		cancelB.setText("Cancel");
+		GridData gridData2 = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_BEGINNING);
+		gridData2.horizontalIndent = 5;
+		cancelB.setLayoutData(gridData2);
+		cancelB.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				selElement = null;
+				shell.close();
+				shell.dispose();
+				
+			}
+		});
+		
+		//We want the buttons to be of equal size...
+		findB.setSize(cancelB.getSize());
+		
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		
+	}
+	
+	/**
+	 * Populates the rationale elements from the rationale database. This is a
+	 * recursive method that calls itself to add new children to the current
+	 * root node.
+	 * @param node - the root node for the tree
+	 * @param parentName - the name of the parent at the root node.
+	 * @param type specify which type of element we should display as treeview.
+	 * @param firstTime flag for whether this is the first time to call this method.
+	 */
+	void populateTree(TreeItem node, String parentName, RationaleElementType type, boolean firstTime )
+	{
+		if (parentName==null)
+			return;
+		RationaleDB d = RationaleDB.getHandle();
+		Vector eleList = d.getElements_TreeView(parentName,type,firstTime);
+		firstTime=false;
+		Enumeration onts = eleList.elements();
+		while (onts.hasMoreElements())
+		{
+			String child = (String)onts.nextElement();
+			TreeItem next = new TreeItem(node, SWT.NONE);
+			next.setText(child);
+			populateTree(next, child, type, firstTime);
+		}
+		
+	}
+	
+	/**
+	 * Get the selected item from the tree
+	 * @return the selected ontology entry
+	 */
+	public RationaleElement getSelEle()
+	{
+		return selElement;
+	}
+	
+}
