70b08c60cec2f87d6a365382c647c62e9daee69d
Janet Burge <burgeje@miamioh.edu>
2009-06-18 23:07:20 +0000

    Used to store node data in a hashtable so it can be put into an XML file when completed.

---
+++ src/edu/wpi/cs/jburge/SEURAT/views/rationaleNode.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs.jburge.SEURAT.views;
+
+public class rationaleNode {
+
+	public String elementType;
+	public String elementContents;
+	
+	public rationaleNode(String t)
+	{
+		elementType=t;
+		elementContents="";
+	}
+}
